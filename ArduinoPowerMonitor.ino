651dcc6557ebda8f4919a7b6cdf7509191cdb05c
Matej Drolc <matej.drolc@pingec.si>
2014-11-25 22:13:27 +0100

    Added support for flow meter Grundfos VFS 5-100 QT

--- ArduinoPowerMonitor.ino
+++ ArduinoPowerMonitor.ino
@@ -4,19 +4,25 @@ EnergyMonitor emon2;             // Create an instance
 EnergyMonitor emon3;             // Create an instance 
 
 //#define CALIBRATION_MODE       // Uncomment for calibration mode (outputs less data)
+#define FLOW_METER             // Uncomment if you have a Grundfos VFS 5-100 QT flow meter connected on A6 (flow) and A7 (temp)
+
+#if defined(FLOW_METER)
+	//vcc adjustment, for flow meter only, emon calculations have their own constant that among other things, adjust for this as well
+	const float flow_vcc_calibration = 1; 
+#endif
 
 void setup()
 {
-  Serial.begin(9600); 
-  
-  emon1.current(0, 29 * 1.08); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
-  emon1.voltage(3, 148, 1.7);  // Voltage: input pin, calibration, phase_shift
-  
-  emon2.current(1, 29 * 1.08); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
-  emon2.voltage(4, 147.5, 1.7);  // Voltage: input pin, calibration, phase_shift
-  
-  emon3.current(2, 29 * 1.075); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
-  emon3.voltage(5, 147.5, 1.7);  // Voltage: input pin, calibration, phase_shift
+Serial.begin(9600); 
+
+	emon1.current(0, 29 * 1.08); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
+	emon1.voltage(3, 148, 1.7);  // Voltage: input pin, calibration, phase_shift
+
+	emon2.current(1, 29 * 1.08); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
+	emon2.voltage(4, 147.5, 1.7);  // Voltage: input pin, calibration, phase_shift
+
+	emon3.current(2, 29 * 1.075); // Current: input pin, calibration. Cur Const= Ratio/BurdenR. 1800/62 = 29.
+	emon3.voltage(5, 147.5, 1.7);  // Voltage: input pin, calibration, phase_shift
 }
 
 void loop()
@@ -24,6 +30,27 @@ void loop()
 	emon1.calcVI(20,2000);          // Calculate all. No.of crossings, time-out
 	emon2.calcVI(20,2000);          // Calculate all. No.of crossings, time-out
 	emon3.calcVI(20,2000);          // Calculate all. No.of crossings, time-out
+
+#if defined(FLOW_METER)
+
+	float vcc = emon1.readVcc() * flow_vcc_calibration / 1000.0;
+
+	int f = analogRead(A6);
+	float flow_ = f * (vcc / 1023.0);
+	int t = analogRead(A7);
+	float temp_ = t * (vcc / 1023.0);
+
+	float flow = (flow_ - 0.375) / 0.03125;
+	float temp = (temp_ - 0.5) / 0.03;
+
+	Serial.print("FM");
+	Serial.print(flow);
+	Serial.print(',');    
+	Serial.print(temp);
+	Serial.print(',');
+	
+#endif
+
 
 #if defined(CALIBRATION_MODE)
 
@@ -42,7 +69,7 @@ void loop()
 	Serial.print(emon2.Irms);
 	Serial.print(',');   
 	Serial.print(emon3.Irms); 
-	
+
 	Serial.print("         "); 
 
 	Serial.print("RealPower: ");
@@ -51,6 +78,7 @@ void loop()
 	Serial.print(emon2.realPower);
 	Serial.print(',');   
 	Serial.println(emon3.realPower); 
+	
 #else
 
 	Serial.print(emon1.realPower);
@@ -84,7 +112,7 @@ void loop()
 	Serial.print(emon3.Irms);
 	Serial.print(',');
 	Serial.println(emon3.powerFactor);  
-	
+
 #endif    
 
 }
--- README.md
+++ README.md
@@ -17,3 +17,21 @@ Measured data will be output to serial port in a format of:
 ###Example output
     1328.1776,1395.2158,229.5877,6.0773,0.9519,166775.26,1947.8288,227.1191,8.577,0.92,1418.4513,1487.4756,230.4413,6.4552,0.953
 
+
+#Flow meter support
+The code also support [Grundfos VFS 5-100 QT]() flow meter connected on pins A6 (flow) and A7 (temp).
+To enable it, uncommet the FLOW_METER define.
+
+**Be aware that enabling this will change the output format!**
+
+##Output (flow meter enabled)
+An easy way to distinguish logs that show flow meter values from those that don't is to check if the line begins with "FM".
+
+Measured data will be output to serial port in a format of:
+
+    FM,flowmeter.flowrate[l/min],flowmeter.temperature[°C],phase1.realPower[Watts], phase1.apparentPower[Watts], phase1.Vrms[Volts], phase1.Irms[Ampers], phase1.powerFactor, phase2.realPower[Watts], phase2.apparentPower[Watts], phase2.Vrms[Volts], phase2.Irms[Ampers], phase2.powerFactor, phase3.realPower[Watts], phase3.apparentPower[Watts], phase3.Vrms[Volts], phase3.Irms[Ampers], phase1.powerFactor
+
+###Example output
+    FM31.24,38.72,1029.57,1465.41,230.07,6.37,0.70,965.36,1406.79,225.80,6.23,0.69,1545.48,2010.47,227.95,8.82,0.77
+
+
